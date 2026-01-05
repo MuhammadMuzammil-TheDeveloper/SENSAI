29805aad7328b14a34da1f22064ad6f12194d536
MuhammadMuzammil-TheDeveloper <muzans786@gmail.com>
2026-01-05 14:07:43 +0500

    Adding Shadcn properties and Dependcies

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
-
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      Welcome to my new project
+      Welcome to my new project boss!
+      <br />
+      <Button> Hello </Button>
+      {/* <Button> The div component should be working now. </Button> */}
     </div>
   );
 }
