d1e56888b9d597a48d91f636eaf78eceb4455ba1
MuhammadMuzammil-TheDeveloper <muzans786@gmail.com>
2026-01-06 19:35:54 +0500

    Add Footer

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "../components/theme-provider";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -22,7 +23,23 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+       <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {/* header */}
+            <main className="min-h-screen"> {children}</main>
+          {/* footer */}
+          <footer className="bg-muted">
+            <div>
+              <p>
+                Made with Muhammad Muzammil
+              </p>
+              </div>
+          </footer>
+          </ThemeProvider>
       </body>
     </html>
   );
