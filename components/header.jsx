95deab7612c3375ae41688ae682bf147eaa538e5
MuhammadMuzammil-TheDeveloper <muzans786@gmail.com>
2026-01-07 09:34:59 +0500

    Add navbar and images

--- components/header.jsx
+++ components/header.jsx
@@ -1,10 +1,51 @@
-import React from 'react';
+// import React from 'react';
+import { SignedIn, SignedOut, UserButton, SignInButton } from "@clerk/nextjs";
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { LayoutDashboard } from "lucide-react";
 
 
 const Header = () => {
   return (
-    <div>Header</div>
-    )
+    <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md">
+
+      <nav>
+
+        <Link href="/">
+          <Image
+            src='/logo.png'
+            alt="Sensai Logo"
+            width={200}
+            height={60}
+            className="h-12 py-1 w-auto object-contain"
+          />
+        </Link>
+        <div>
+          <SignedIn>
+            <Link href={"/dashboard"}>
+              <Button>
+                <LayoutDashboard className="h-4 w-4" />
+                Industry Insights
+              </Button>
+            </Link>
+          </SignedIn>
+        </div>
+      </nav>
+
+
+
+
+      <SignedOut>
+        <SignInButton />
+
+      </SignedOut>
+
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  )
 }
 
 export default Header;
