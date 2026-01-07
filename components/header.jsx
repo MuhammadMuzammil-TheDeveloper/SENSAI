8cb1e3aac2979ac0185c015f5e8ed7d4653192f6
MuhammadMuzammil-TheDeveloper <muzans786@gmail.com>
2026-01-07 11:09:14 +0500

    Initialize navbar button & Functionality

--- components/header.jsx
+++ components/header.jsx
@@ -4,13 +4,26 @@ import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { LayoutDashboard } from "lucide-react";
-
+import { StarsIcon } from "lucide-react";
+import { ChevronDown } from "lucide-react";
+import { FileText } from "lucide-react";
+import { PenBox } from "lucide-react";
+import { GraduationCap } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+  DropdownMenuItem
+} from "@/components/ui/dropdown-menu";
 
 const Header = () => {
   return (
-    <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md">
+    <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md
+    z-50 supports-[backdrop-filter]:bg-background/60">
 
-      <nav>
+      <nav className=" container mx-auto px-4 h-16 flex items-center justify-between">
 
         <Link href="/">
           <Image
@@ -21,29 +34,57 @@ const Header = () => {
             className="h-12 py-1 w-auto object-contain"
           />
         </Link>
-        <div>
+        <div className="flex items-center space-x-2 md:space-x-4">
           <SignedIn>
             <Link href={"/dashboard"}>
-              <Button>
+              <Button variant="outline">
                 <LayoutDashboard className="h-4 w-4" />
-                Industry Insights
+                <span className="hidden sm:block">
+                  Industry Insights
+                </span>
               </Button>
             </Link>
           </SignedIn>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Button>
+                <StarsIcon className="h-4 w-4" />
+                <span className="hidden sm:block">Growth Tools</span>
+                <ChevronDown className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>
+                <Link href={"/resume"} className="flex items-center gap-2">
+                  <FileText className="h-4 w-4" />
+                  <span>Build Resume</span>
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link href={"/ai-cover-letter"} className="flex items-center gap-2">
+                  <PenBox className="h-4 w-4" />
+                  <span>Cover Letter</span>
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link href={"/interview"} className="flex items-center gap-2">
+                  <GraduationCap className="h-4 w-4" />
+                  <span>Interview Prep</span>
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+          <SignedOut>
+            <SignInButton />
+              <Button variant="outline"> Sign In</Button>
+            <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+          </SignedIn>
         </div>
       </nav>
-
-
-
-
-      <SignedOut>
-        <SignInButton />
-
-      </SignedOut>
-
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
     </header>
   )
 }
